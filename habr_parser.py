411467099548a8ddbff54cae37421a748aa6a108
trb1maker <tsiporinalex@gmail.com>
2022-11-21 20:16:46 +0500

    Добавил парсинг даты публикации статьи.

--- habr_parser.py
+++ habr_parser.py
@@ -1,8 +1,10 @@
 from collections import namedtuple
 from bs4 import BeautifulSoup
 import requests
+import datetime
 
-Article = namedtuple('Article', ('article_id', 'header', 'tags', 'text'))
+
+Article = namedtuple('Article', ('article_id', 'header', 'tags', 'text', 'public_datetime'))
 
 
 def parse_article(article_id: int) -> Article:
@@ -13,12 +15,11 @@ def parse_article(article_id: int) -> Article:
 
     soup = BeautifulSoup(response.text)
 
-    header = soup.find(name='h1') \
-                 .get_text()
+    return Article(
+        article_id,                                                                                               # article_id
+        soup.find(name='h1').get_text(),                                                                          # header
+        [tag.get_text().lower() for tag in soup.find_all(name='li', attrs={'class': 'tm-separated-list__item'})], # tags
+        soup.find(name='div', attrs={'class': 'article-formatted-body'}).get_text(),                              # text
+        datetime.datetime.strptime(soup.find(name='time').get('datetime'), '%Y-%m-%dT%H:%M:%S.000Z')              # public_datetime
+    )
 
-    tags = [tag.get_text().lower() for tag in soup.find_all(name='li', attrs={'class': 'tm-separated-list__item'})]
-
-    text = soup.find(name='div', attrs={'class': 'article-formatted-body'}) \
-               .get_text()
-
-    return Article(article_id, header, tags, text)
