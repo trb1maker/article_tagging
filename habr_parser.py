4aedf4303b0b06c16118ff389d497bfa3237acd4
trb1maker <tsiporinalex@gmail.com>
2022-11-22 11:54:22 +0500

    parse_habr_testing.ipynb удален.

--- habr_parser.py
+++ habr_parser.py
@@ -28,50 +28,54 @@ def parse_article(article_id: int) -> Article:
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, features='html.parser')
 
+        header = soup.find(name='h1') \
+                     .get_text()
+
+        tags = [tag.get_text().lower() for tag in soup.find_all(
+                name='li', attrs={'class': 'tm-separated-list__item'})]
+
+        text = soup.find(name='div', attrs={'class': 'article-formatted-body'}) \
+                   .get_text()
+
+        public_datetime = datetime.datetime.strptime(soup.find(name='time').get(
+            'datetime'), '%Y-%m-%dT%H:%M:%S.000Z')
+
+        rate = soup.find(name='span', attrs={'class': 'tm-votes-meter__value'}) \
+                   .get_text()\
+                   .strip()
+
+        view = soup.find(name='span', attrs={'class': 'tm-icon-counter__value'}) \
+                   .get_text() \
+                   .strip()
+
+        bookmark = soup.find(name='span', attrs={'class': 'bookmarks-button__counter'}) \
+                       .get_text() \
+                       .strip()
+
+        try:
+            # Если количество комментариев равно 0, то при парсинге число почему-то опускатеся.
+            # Я не смог разобраться, почему так происходит и обернул это все в try / except
+            
+            comment = soup.find(name='span', attrs={'class': 'tm-article-comments-counter-link__value'}) \
+                          .get_text() \
+                          .strip() \
+                          .split(' ')[1]
+        except IndexError:
+            comment = '0'
+
+        status = 'ok'
+
         return Article(
-
-            # article_id
             article_id,
-
-            # header
-            soup.find(name='h1') \
-                .get_text(),
-
-            # tags
-            [tag.get_text().lower() for tag in soup.find_all(
-                name='li', attrs={'class': 'tm-separated-list__item'})],
-
-            # text
-            soup.find(name='div', attrs={'class': 'article-formatted-body'}) \
-                .get_text(),
-
-            # public_datetime
-            datetime.datetime.strptime(soup.find(name='time').get(
-                'datetime'), '%Y-%m-%dT%H:%M:%S.000Z'),
-
-            # rate
-            soup.find(name='span', attrs={'class': 'tm-votes-meter__value'}) \
-                .get_text()\
-                .strip(),
-
-            # view
-            soup.find(name='span', attrs={'class': 'tm-icon-counter__value'}) \
-                .get_text() \
-                .strip(),
-
-            # bookmark
-            soup.find(name='span', attrs={'class': 'bookmarks-button__counter'}) \
-                .get_text() \
-                .strip(),
-
-            # comment
-            soup.find(name='span', attrs={'class': 'tm-article-comments-counter-link__value'}) \
-                .get_text() \
-                .strip() \
-                .split(' ')[1],
-
-            # status
-            'ok'
+            header,
+            tags,
+            text,
+            public_datetime,
+            rate,
+            view,
+            bookmark,
+            comment,
+            status
         )
 
     elif response.status_code == 404:
@@ -81,3 +85,15 @@ def parse_article(article_id: int) -> Article:
 
     else:
         raise Exception(f'Ошибка сервера: {response.status_code}')
+
+
+# Небольшая демонстрация
+if __name__ == '__main__':
+    
+    first_article = 1
+    
+    article = parse_article(first_article)
+
+    print(f'Самая первая статья на Хабре на тему {article.header} опубликована {article.public_datetime.strftime("%d.%m.%Y")}')
+    print(f'\n{article.text}\n')
+    print(article.tags)
