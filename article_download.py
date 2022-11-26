c75c8228da73b9fc8f0204e59c90473eab77f1e6
trb1maker <tsiporinalex@gmail.com>
2022-11-26 23:55:34 +0500

    Reverse download batch to csv.

--- article_download.py
+++ article_download.py
@@ -1,16 +1,11 @@
-from collections import namedtuple
-from typing import Optional
-
 from bs4 import BeautifulSoup
 import requests
 
 import datetime
-import random
+
 import time
+
 import csv
-
-
-Article = namedtuple('Article', ('id', 'header', 'tags', 'text', 'date', 'rate', 'view', 'bookmark', 'comment'))
 
 
 def parse_view(view: str) -> int:
@@ -26,7 +21,7 @@ def parse_view(view: str) -> int:
     return int(float(view) * k) 
 
 
-def parse_article(id: int) -> Optional[Article]:
+def get_article(id: int) -> tuple:
     url = f'https://habr.com/ru/post/{id}/'
 
     response = requests.get(url)
@@ -39,14 +34,19 @@ def parse_article(id: int) -> Optional[Article]:
     header = soup.find(name='h1') \
                  .get_text()
 
-    tags = [tag.get_text().lower() for tag in soup.find_all(
-            name='li', attrs={'class': 'tm-separated-list__item'})]
+    tags = [tag.get_text().lower().strip() for tag in soup.find_all(name='li', attrs={'class': 'tm-separated-list__item'})]
 
     text = soup.find(name='div', attrs={'class': 'article-formatted-body'}) \
                .get_text()
 
-    date = datetime.datetime.strptime(soup.find(name='time').get(
-        'datetime'), '%Y-%m-%dT%H:%M:%S.000Z')
+    
+    date = soup.find(name='time').get('datetime')
+    
+    try:
+        date = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.000Z')
+    except:
+        # Если формат даты неожиданный, сохраню исходные данные        
+        pass
 
     rate = int(soup.find(name='span', attrs={'class': 'tm-votes-meter__value'})
                    .get_text()
@@ -61,43 +61,47 @@ def parse_article(id: int) -> Optional[Article]:
                        .strip())
 
     try:
-        # Если количество комментариев равно 0, то при парсинге число почему-то опускатеся.
-        # Я не смог разобраться, почему так происходит и обернул это все в try / except
-
         comment = int(soup.find(name='span', attrs={'class': 'tm-article-comments-counter-link__value'})
                           .get_text()
                           .strip()
                           .split(' ')[1])
     except IndexError:
+        # Если количество комментариев равно 0, то при парсинге число почему-то опускатеся.
+        # Я не смог разобраться, почему так происходит и обернул это все в try / except
         comment = 0
 
-    return Article(id, header, tags, text, date, rate, view, bookmark, comment)
+    return (id, header, tags, text, date, rate, view, bookmark, comment)
 
 
-def get_articles(start_id: int):
-    stop_id = start_id + 10_000
+def get_articles_batch(last_id: int, batch=10_000) -> int:
+    start_id = last_id
+    stop_id = last_id - batch
 
-    file_name = f'data_{stop_id}.csv'
+    file_name = f"data_{datetime.datetime.now().strftime('%Y-%m-%d')}_{last_id}.csv"
 
     with open(file_name, 'w') as f:
         w = csv.writer(f)
 
-        for id in range(start_id, stop_id):
-            article = parse_article(id)
-
-            if article:
-                w.writerow(article)
+        for id in range(start_id, stop_id, -1):
+            try:
+                article = get_article(id)
+                
+                if article:
+                    w.writerow(article)
+            except:
+                print(f'Ошибка парсинга статьи {id}')
             
-            time.sleep(random.randint(1, 3) * 0.3)
+            time.sleep(0.5)
+    
+    return stop_id
 
 
 if __name__ == '__main__':
-    start_id = 1
+    last_id = 701_600 # ID последней статьи, опубликованной 2022-11-26
 
-    while start_id <= 700_000:
-        print(f'Скачиваю и сохранаяю данные {start_id} - {start_id + 10_000}')
+    while True:
+        print(f"{datetime.datetime.now().strftime('%Y-%m-%d [%H:%M]')}\t{last_id}")
         
-        get_articles(start_id)
-        
-        time.sleep(60)
-        start_id += 10_000
+        last_id = get_articles_batch(last_id)
+        time.sleep(60 * 5)
+
