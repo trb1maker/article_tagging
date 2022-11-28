2280ce580de1cd1c3f65d7ace469e5457a228005
trb1maker <tsiporinalex@gmail.com>
2022-11-28 20:17:37 +0500

    tags.py: изменена tag_elements

--- utils/tags.py
+++ utils/tags.py
@@ -2,13 +2,13 @@ import re
 
 remove_pattern = r'[^a-zа-я. -]'
 split_pattern = r'[- .]'
-min_tag_lenght = 3
+min_tag_lenght = 2
 
 
-def tag_elements(tag: str) -> list:
-    '''Разделяет очищенный тэг на составляющие.'''
-    
-    return [tag for tag in re.split(split_pattern, re.sub(remove_pattern, '', tag)) if len(tag) >= min_tag_lenght]
+def tag_elements(tag: str) -> str:
+    '''Очищает тег'''
+
+    return ' '.join([tag for tag in re.split(split_pattern, re.sub(remove_pattern, '', tag)) if len(tag) >= min_tag_lenght])
 
 
 if __name__ == '__main__':
