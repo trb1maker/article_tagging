aec965c41508d7804f86b6ac19785de662395484
trb1maker <tsiporinalex@gmail.com>
2022-11-28 10:35:41 +0500

    fix utils.tag_elements

--- utils/tags.py
+++ utils/tags.py
@@ -1,13 +1,15 @@
 import re
 
-SPLIT_SYMBOLS = r'[ -.]'
-REMOVE_PATTERN = r'^[\W\d]|[\W\d]$'
+remove_pattern = r'[^a-zа-я. -]'
+split_pattern = r'[- .]'
+min_tag_lenght = 3
 
 
 def tag_elements(tag: str) -> list:
-    '''
-    Разделяет очищенный тэг на составляющие.
-    '''
-    tag = re.sub(string=tag, repl='', pattern=REMOVE_PATTERN)
+    '''Разделяет очищенный тэг на составляющие.'''
+    
+    return [tag for tag in re.split(split_pattern, re.sub(remove_pattern, '', tag)) if len(tag) >= min_tag_lenght]
 
-    return [elm for elm in re.split(string=tag, pattern=SPLIT_SYMBOLS) if len(elm) > 2]
+
+if __name__ == '__main__':
+    pass
